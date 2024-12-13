8435446592a7af7e8903adece4229136a3d3c1ca
ChadAlpacatLover <ofirfirsa@gmail.com>
2024-12-13 16:39:52 +0200

    creating merge branch

---
+++ Assets/Scripts0/Merging/ItemScriptable.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ItemScriptable", menuName = "Scriptable Objects/ItemToMerge")]
+public class ItemScriptable : ScriptableObject
+{
+    public int itemID;
+    public Sprite sprite;
+
+}
